56f917b9339d944cda7f00b6dfdb7f07ad0889c2
singeev <a.singeev@gmail.com>
2016-04-17 14:45:23 +0300

    finished with LoadTest

--- src/test/java/com/singeev/bank/tests/LoadIntegtationTest.java
+++ src/test/java/com/singeev/bank/tests/LoadIntegtationTest.java
@@ -1,6 +1,7 @@
 package com.singeev.bank.tests;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 import com.goebl.david.Webb;
 import com.singeev.bank.dao.Account;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.sql.DataSource;
 
@@ -67,54 +69,63 @@ public class LoadIntegtationTest {
         System.out.println("DataBase prepared for test and contains " + list.size() + " accounts.");
     }
 
-    @Test // тоже работает, на другой библиотеке (объект создается прямо внутри метода), с ExecutorService не работает
-    public void test() {
-        int fromid = list.get(rand1.nextInt(19)).getId();
-        int toid = list.get(rand2.nextInt(19)).getId();
-        int summ = 1000;
-        Webb webb = Webb.create();
-        webb.post("http://localhost:8080/lpbank-1.0/transfer")
-                .param("fromid", fromid)
-                .param("toid", toid)
-                .param("summ", summ)
-                .asVoid();
-    }
-
-
-    @Test // не работает
-    public void isRequestWorking() {
+    // WORKS: makes 50 threads pool, makes 50 threads, make 1000 POST requests in each thread via for loop
+    // waits 2 minutes for termination
+    @Test
+    public void isRequestWorking() throws InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(50);
         for (int i = 0; i < 50; i++) {
             executorService.execute(new Runnable() {
                 @Override
                 public void run() {
-                    int fromid = list.get(rand1.nextInt(19)).getId();
-                    int toid = list.get(rand2.nextInt(19)).getId();
-                    int summ = 1000;
-                    given().parameters("fromid", fromid, "toid", toid, "summ", summ)
-                            .when().post().then().assertThat().statusCode(302);
-                    System.out.println("Transferred " + summ + "$ from id[" + fromid + "] to id[" + toid + "].");
+                    for (int j = 0; j < 1000; j++) {
+                        int fromid = list.get(rand1.nextInt(19)).getId();
+                        int toid = list.get(rand2.nextInt(19)).getId();
+                        int summ = 1000;
+                        given().parameters("fromid", fromid, "toid", toid, "summ", summ)
+                                .when().post().then().assertThat().statusCode(302);
+                        System.out.println("Transferred " + summ + "$ from id[" + fromid + "] to id[" + toid + "].");
+                    }
                 }
             });
         }
         executorService.shutdown();
+        executorService.awaitTermination(2, TimeUnit.MINUTES);
+
+        System.out.println("Finished with requests, calculating results...");
+        // checking results
+        int summBalance = 0;
+        for (Account account : list) {
+            summBalance += account.getBalance();
+        }
+        assertEquals("Should be 2000000.", 2000000, summBalance);
+        System.out.println("All good: money wasn't lost!");
     }
 
-
-    @Test //РАБОТАЕТ: делает 50 успешных запросов ПОСТ за счет цикла
+    //WORKS: makes POST requests in for loop without multithreading.
+    @Test
     @Ignore
     public void loadWithoutThreads() {
         for (int i = 0; i < 50; i++) {
             int fromid = list.get(rand1.nextInt(19)).getId();
             int toid = list.get(rand2.nextInt(19)).getId();
-            int summ = 1000;
+            int summ = 100;
             given().parameters("fromid", fromid, "toid", toid, "summ", summ)
                     .when().post().then().assertThat().statusCode(302);
             System.out.println("Transferred " + summ + "$ from id[" + fromid + "] to id[" + toid + "].");
         }
+        System.out.println("Finished with requests, calculating results...");
+        // checking results
+        int summBalance = 0;
+        for (Account account : list) {
+            summBalance += account.getBalance();
+        }
+        assertEquals("Should be 2000000.", 2000000, summBalance);
+        System.out.println("All good: money wasn't lost!");
     }
 
-    @Test // РАБОТАЕТ: но, похоже, что не в параллельных потоках (чтобы его запустить нужно раскоментить @Rule)
+    // WORKS, but looks like not in parallel threads. To run this test you should uncomment @Rule annotation.
+    @Test
     @Ignore
     @PerfTest(threads = 50)
     public void testBloadTest() throws Exception {
@@ -127,14 +138,37 @@ public class LoadIntegtationTest {
                     .when().post().then().assertThat().statusCode(302);
             System.out.println(count++ + " - Transferred " + summ + "$ from id[" + fromid + "] to id[" + toid + "].");
         }
-    }
-
-    @After
-    public void checkBalances() {
+        System.out.println("Finished with requests, calculating results...");
+        // checking results
         int summBalance = 0;
         for (Account account : list) {
             summBalance += account.getBalance();
         }
-        System.out.println("Should be 2000000. In fact: " + summBalance);
+        assertEquals("Should be 2000000.", 2000000, summBalance);
+        System.out.println("All good: money wasn't lost!");
+    }
+
+    // WORKS as well. Based on http://hgoebl.github.io/DavidWebb/ library.
+    // in this implementation - without multithreading
+    @Test
+    @Ignore
+    public void test() {
+        int fromid = list.get(rand1.nextInt(19)).getId();
+        int toid = list.get(rand2.nextInt(19)).getId();
+        int summ = 1000;
+        Webb webb = Webb.create();
+        webb.post("http://localhost:8080/lpbank-1.0/transfer")
+                .param("fromid", fromid)
+                .param("toid", toid)
+                .param("summ", summ)
+                .asVoid();
+        System.out.println("Finished with requests, calculating results...");
+        // checking results
+        int summBalance = 0;
+        for (Account account : list) {
+            summBalance += account.getBalance();
+        }
+        assertEquals("Should be 2000000.", 2000000, summBalance);
+        System.out.println("All good: money wasn't lost!");
     }
 }
